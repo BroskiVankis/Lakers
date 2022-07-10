36f380e9f4a2bfb1529169c724f9cc8706163b55
BroskiVankis <ivanov.vankis@gmail.com>
2022-07-10 17:22:35 +0300

    First commit of personal project. DB created with some front-end.

--- src/main/java/com/example/lakers/init/DatabaseInitializer.java
+++ src/main/java/com/example/lakers/init/DatabaseInitializer.java
@@ -1,20 +1,20 @@
-//package com.example.lakers.init;
-//
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class DatabaseInitializer implements CommandLineRunner {
-//
-//    private final CategoryService categoryService;
-//
-//    public DatabaseInitializer(CategoryService categoryService) {
-//        this.categoryService = categoryService;
-//    }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        categoryService.initCategories();
-//    }
-//
-//}
+package com.example.lakers.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseInitializer implements CommandLineRunner {
+
+    private final CategoryService categoryService;
+
+    public DatabaseInitializer(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        categoryService.initCategories();
+    }
+
+}
